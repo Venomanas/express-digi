82ec7817df6dc07c70dd91d9535c11ac05ae9227
anas <anassayyed000@gmail.com>
2025-01-13 00:05:06 +0530

    added usage

--- index.js
+++ index.js
@@ -10,12 +10,36 @@
 // app.get("/ice-tea", (req, res) => {
 //   res.send("hello from Anas and his tea");
 // });
+
 import "dotenv/config";
+import logger from "./logger.js";
+import morgan from "morgan";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
+//small middleware
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  }));
 
 let teaData = [];
 let nextId = 1;
