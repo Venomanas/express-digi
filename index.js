90f985d1ad6c1e59a8871d948e077bb0f2b2f162
anas <anassayyed000@gmail.com>
2025-01-12 23:32:36 +0530

    add env files to .ignore and update dependencies

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // basic
 
 // app.get("/", (req, res) => {
@@ -11,11 +10,11 @@
 // app.get("/ice-tea", (req, res) => {
 //   res.send("hello from Anas and his tea");
 // });
-
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 let teaData = [];
@@ -40,30 +39,29 @@ app.get("/teas/:id", (req, res) => {
   res.status(200).send(tea);
 });
 
-//update 
+//update
 
-app.put('/teas/:id',(req, res) => {
- const tea = teaData.find(t => t.id === parseInt(req.params.id));
- if(!tea){
-   return res.status(404).send('Tea not found');
- }
- const {name, price} = req.body;
- tea.name = name
- tea.price = price
- res.send(200).send(tea)
-})
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find(t => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
+});
 
 // delete
 // we cannot delete the tea data because there is a different way of writing it ( " we need to apply findIndex instead of find ike other operation ")
-app.delete('/teas/:id',(req, res) => {
+app.delete("/teas/:id", (req, res) => {
   const teaIndex = teaData.findIndex(t => t.id === parseInt(req.params.id));
-  if(teaIndex === -1){
-    return res.status(404).send('Tea not found');
+  if (teaIndex === -1) {
+    return res.status(404).send("Tea not found");
   }
   teaData.splice(teaIndex, 1);
   res.send(204).send();
-})
-
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}....`);
